3df748845c5d00af9d27d16693213be0de115336
daniel.ramirez.munoz.contractor <daniel.ramirez.munoz.contractor@bbva.com>
2023-11-06 16:14:55 -0600

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), // 12371368942234
+            date: new Date().toDateString(), // mond 06, noviembre
+            text: 'Et proident est et do aliqua in aliquip fugiat non. Nisi nostrud et incididunt nisi non mollit deserunt duis. Culpa sit proident do duis est enim quis minim et. Sint labore ut ipsum veniam eu officia tempor exercitation nostrud est esse non laboris. Irure esse sint consectetur sint eiusmod adipisicing labore non velit veniam occaecat.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 1000, // 12371368942234
+            date: new Date().toDateString(), // mond 06, noviembre
+            text: 'Consequat velit sint officia voluptate esse tempor quis cupidatat velit reprehenderit. Veniam ipsum aute ea Lorem officia est cupidatat et. Consectetur et eiusmod occaecat eiusmod sunt nostrud sit aute ad adipisicing minim deserunt velit. Consequat irure elit aliqua non. Ipsum ea cillum in culpa est aliquip in laborum id consequat. Qui ullamco id nulla nostrud occaecat fugiat. Sit cillum ullamco consectetur et deserunt proident fugiat et ut quis adipisicing fugiat labore ad.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 2000, // 12371368942234
+            date: new Date().toDateString(), // mond 06, noviembre
+            text: 'Ea adipisicing ut enim sint do laborum elit ex excepteur quis ad adipisicing aute ullamco. Dolor in adipisicing do minim. Enim incididunt eu velit amet. Velit dolor exercitation et amet pariatur eu magna. Consequat tempor sunt est dolor aliqua cupidatat. Adipisicing esse cupidatat laboris laborum Lorem ad dolore ut ea sint ea ex consectetur.',
+            picture: null, // https://
+        },
+    ]
+})
